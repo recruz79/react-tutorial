49a27b55255cc95c0d1f65d6f1af8909d9af8359
recruz79 <raul.cruz79@gmail.com>
2020-06-07 14:01:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        I want to learon a lot more wow this is so fast that I want to do all my work here!!!
+        First thing is to get use to create component so I can reuse them for example the blog compoents shold be able to use them anytime
+        lets try to add them into this project  
+      </header>
+    </div>
+  );
+}
+
+export default App;
